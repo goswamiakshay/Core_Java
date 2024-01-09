bf498513b0205ffb10af9265b96819f86c9f0872
Akshay Goswami <44131661+goswamiakshay@users.noreply.github.com>
2024-01-09 12:01:39 +0530

    Add files via upload

---
+++ Reverse_Number.java
@@ -0,0 +1,31 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Reverse_Number {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);       
+		System.out.print("Enter the Digits :");
+		int num = sc.nextInt(); 
+		int rev = 0;
+		int rem = 0;
+		int temp=num;
+		while(num>0)
+		{
+			rem = num % 10;
+			rev = rev * 10 + rem;
+			num= num/ 10;
+		}
+		System.out.println("Reverse Number :" + rev);
+		if(temp==rev){
+			System.out.println("This numbe is Palindrome.");
+		}
+		else {
+			System.out.println("This is not Palindrome.");
+		}
+	}
+		
+	}
+
+
